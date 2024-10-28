880cf947e0d4924c1b1cca50a7d6dbef20ccac8a
shajalal07s <khanranu81@gmail.com>
2024-10-29 01:02:03 +0600

    font-family and color customize from tailwind-config-file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#222831',
+        'dep-sky': '#7394c6',
+        'text-color': '#00ADB5',
+        'visiable': '#393E4680',
+        'input': '#333842',
+        'Social-icon': '#393E46',
+      },
+      fontFamily : {
+        'header': 'Poppins',
+        'logo': 'oswald' 
+      }
+    },
   },
   plugins: [],
 }
