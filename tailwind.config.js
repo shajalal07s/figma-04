40cdfd520cb81d42916c9fa0ec25b4614829f1fb
Mohammad Shajalal <khanranu81@gmail.com>
2024-11-19 00:39:03 +0600

    tailwind setup succesfully and page update and img add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
@@ -10,12 +10,21 @@ module.exports = {
         'visiable': '#393E4680',
         'input': '#333842',
         'Social-icon': '#393E46',
+        'color-white-dark': 'rgba(238, 238, 238, 0.1)',
+        'color-gray-dark': 'rgba(57, 62, 70, 0.75)',
       },
       fontFamily : {
         'header': 'Poppins',
         'logo': 'oswald' 
       }
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '40px',
+        md: '50px',
+      }
+    }
   },
   plugins: [],
 }
